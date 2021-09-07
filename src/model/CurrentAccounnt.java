cd40f2f0632d3905023e09fc7cb83f201123690e
Deisy Carolina Monroy Gutiérrez <86481741+dcmg03@users.noreply.github.com>
2021-09-07 13:35:02 -0500

    Avances

--- src/model/CurrentAccounnt.java
+++ src/model/CurrentAccounnt.java
@@ -1,4 +1,22 @@
 package model;
 
-public class CurrentAccounnt {
+import java.time.LocalDate;
+
+public class CurrentAccounnt extends Account{
+    private double overdraft;
+    public CurrentAccounnt(String number, double residue, LocalDate dateCreation, double overdraft) {
+        super(number, residue, dateCreation);
+        this.overdraft=overdraft;
+    }
+
+    public double getOverdraft() {
+        return overdraft;
+    }
+
+    public void setOverdraft(double overdraft) {
+        this.overdraft = overdraft;
+    }
+    public void addCheckBook(String message, String message1){
+        //boolean
+    }
 }
--- src/model/DepositAccount.java
+++ src/model/DepositAccount.java
@@ -1,4 +1,13 @@
 package model;
 
-public class DepositAccount {
+import java.time.LocalDate;
+
+public class DepositAccount extends Account{
+    private int minResidue;
+    private float INTEREST_RATE;
+    public DepositAccount(String number, double residue, LocalDate dateCreation, int minResidue, float INTEREST_RATE) {
+        super(number, residue, dateCreation);
+        this.minResidue=minResidue;
+        this.INTEREST_RATE= (float) 0.05;
+    }
 }
