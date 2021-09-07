dafe56941c2f6bc5369bac06f192fc191740bffc
Deisy Carolina Monroy Gutiérrez <86481741+dcmg03@users.noreply.github.com>
2021-09-07 12:06:01 -0500

    Version 1

---
+++ src/model/Account.java
@@ -0,0 +1,9 @@
+package model;
+
+import java.time.LocalDate;
+
+public abstract class Account {
+    protected String number;
+    protected double residue;
+    protected LocalDate dateCreation;
+}
